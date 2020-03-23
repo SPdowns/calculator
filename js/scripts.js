9476e1b4e9dd7bd772d14ae0db9d79e88422bbf3
Sean Downs <spdowns@protonmail.com>
2020-03-23 13:15:36 -0700

    add subtract function

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
+var subtract = function(number1, number2){
+  return number1 - number2
+}
 var add = function(number1, number2) {
   return number1 + number2;
   }
   
-  alert(add(10,5));
+  // alert(add(10,5));
   
   var number1 = parseInt(prompt("Enter a number"))
   var number2 = parseInt(prompt("Enter 2 number"))
-  var result = add(number1, number2)
-  alert(result);
+  var result = subtract(number1, number2)
+  alert(result)
+
+   number1 = parseInt(prompt("Enter a number"))
+   number2 = parseInt(prompt("Enter 2 number"))
+  result = add(number1, number2)
+  alert(result)
