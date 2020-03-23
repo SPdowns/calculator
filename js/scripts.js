3ae1d8fb8c87212d0bf6febfe1f1b5f69cff826b
Sean Downs <spdowns@protonmail.com>
2020-03-23 13:27:20 -0700

    multiply divde function. git add .

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
-var subtract = function(number1, number2){
+var subtract = function(number1, number2) {
   return number1 - number2
 }
 var add = function(number1, number2) {
   return number1 + number2;
-  }
-  
+}
+var multiply = function(number1, number2) {
+  return number1 * number2;
+}
+var divide = function(number1, number2) {
+  return number1 / number2
+}
   // alert(add(10,5));
   
   var number1 = parseInt(prompt("Enter a number"))
@@ -12,7 +17,17 @@ var add = function(number1, number2) {
   var result = subtract(number1, number2)
   alert(result)
 
-   number1 = parseInt(prompt("Enter a number"))
-   number2 = parseInt(prompt("Enter 2 number"))
+  var number1 = parseInt(prompt("Enter a number"))
+  var number2 = parseInt(prompt("Enter 2 number"))
   result = add(number1, number2)
+  alert(result)
+
+  var number1 = parseInt(prompt("Enter a number"))
+  var number2 = parseInt(prompt("Enter 2 number"))
+  result = multiply(number1, number2)
+  alert(result)
+
+  var number1 = parseInt(prompt("Enter a number"))
+  var number2 = parseInt(prompt("Enter 2 number"))
+  result = parseInt(divide(number1, number2))
   alert(result)
