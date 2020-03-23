67b0f441e079542403721a434d52f87ab69ce5ea
Sean Downs <spdowns@protonmail.com>
2020-03-23 13:27:55 -0700

    multiply divide function work

--- js/scripts.js
+++ js/scripts.js
@@ -29,5 +29,5 @@ var divide = function(number1, number2) {
 
   var number1 = parseInt(prompt("Enter a number"))
   var number2 = parseInt(prompt("Enter 2 number"))
-  result = parseInt(divide(number1, number2))
+  result = Math.round(divide(number1, number2))
   alert(result)
